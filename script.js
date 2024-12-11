3141304845af28ee9a548d92ec821c8e0cd666b0
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2024-12-10 20:42:14 -0600

    Estructurando la pagina

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// script.js
+
 function readMore() {
     alert("Funcionalidad de 'Leer más' aún en desarrollo. ¡Gracias por visitar!");
   }
   
+
+  // Inserta el menú
+  fetch('menu.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('menu-container').innerHTML = data;
+    });
+
+  // Inserta el footer
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-container').innerHTML = data;
+    });
