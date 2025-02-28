5f8f3d939330983fd48984d0e5d8358f9458e52c
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-02-27 22:36:24 -0600

    Trabajando en el menu

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,10 +1,4 @@
-    // Seleccionar elementos
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-    
-    // Evento click en el botón hamburguesa
-    hamburger.addEventListener('click', () => {
-      // Alternar clase 'responsive' para mostrar u ocultar el menú
-      navLinks.classList.toggle('responsive');
-    });
-    
+document.getElementById('hamburger').addEventListener('click', function() {
+  document.querySelector('.left-menu').classList.toggle('active');
+  document.querySelector('.right-menu').classList.toggle('active');
+});
