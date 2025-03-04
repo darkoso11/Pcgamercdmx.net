1df149e0dad864b0722d348b6af9c931a4a8a837
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-03-04 17:17:06 -0600

    Arreglando el menu y los servicios

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,20 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const hamburger = document.getElementById('hamburger');
-  const leftMenu = document.querySelector('.left-menu');
-  const rightMenu = document.querySelector('.right-menu');
-  const menuOverlay = document.getElementById('menuOverlay');
-  
-  hamburger.addEventListener('click', function() {
-    this.classList.toggle('active');
-    leftMenu.classList.toggle('active');
-    rightMenu.classList.toggle('active');
-    menuOverlay.classList.toggle('active');
+/* document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const leftMenu = document.querySelector(".left-menu");
+  const rightMenu = document.querySelector(".right-menu");
+
+  hamburger.addEventListener("click", () => {
+     leftMenu.classList.toggle("active");
+     rightMenu.classList.toggle("active");
   });
-  
-  menuOverlay.addEventListener('click', function() {
-    hamburger.classList.remove('active');
-    leftMenu.classList.remove('active');
-    rightMenu.classList.remove('active');
-    this.classList.remove('active');
-  });
-});
+}); */
