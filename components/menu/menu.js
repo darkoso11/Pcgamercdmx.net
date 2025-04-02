1d7ffc269326850ee91ac9571c4673086530e6eb
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-04-01 23:54:37 -0600

    Ya casi tengo el menu!

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,43 +1,28 @@
 export function initializeMenu() {
     console.log('initializeMenu ejecutado');
-    let menu = null;
-    let success = false;
-    let trys = 0;
-    const intervalId = setInterval(() => {
-        trys++;
-        menu = document.getElementById('menu'); // Verifica que el ID es correcto
-        console.log('Elemento del menú:', menu);
-        console.log('Intentos:', trys);
-        if (menu !== null && success === false) {
-            success = true;
-            clearInterval(intervalId);
-            const hamburger = document.getElementById("hamburger");
-            const menuMovil = document.querySelector(".menu-movil");
-            console.log('se encontro el menu movil', menuMovil)
-            if (!hamburger || !menuMovil) {
-                console.error("No se encontraron los elementos del menú");
-                console.error('No se consiguio', menuMovil)
-                return;
-            }
-            // Alternar clases para mostrar u ocultar el menú
-            hamburger.addEventListener("click", function () {
-                console.log('Hamburguesa clickeada');
-                menuMovil.classList.toggle("active");
-            });
-            console.log('Menú inicializado. 1/2');
 
-            // Cerrar menú cuando se hace clic en un enlace
-            document.querySelectorAll(".menu-movil").forEach(link => {
-                link.addEventListener("click", function () {
-                    menuMovil.classList.remove("active");
+    const hamburger = document.getElementById("hamburger");
+    const menuMovil = document.querySelector(".menu-movil");
 
-                });
-            });
-            console.log('Menú inicializado. 2/2');
-        }
-        if (trys >= 10) {
-            clearInterval(intervalId);
-            console.error('No se encontró el menú');
-        }
-    }, trys === 0 ? 0 : 1000 * (trys + 1));
+    if (!hamburger || !menuMovil) {
+        console.error("No se encontraron los elementos del menú");
+        return;
+    }
+
+    // Alternar clases para mostrar u ocultar el menú
+    hamburger.addEventListener("click", function () {
+        console.log('Hamburguesa clickeada');
+        menuMovil.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    });
+
+    // Cerrar menú cuando se hace clic en un enlace
+    document.querySelectorAll(".menu-movil a").forEach(link => {
+        link.addEventListener("click", () => {
+            menuMovil.classList.remove("active");
+            hamburger.classList.remove("active");
+        });
+    });
+
+    console.log('Menú inicializado.');
 }
