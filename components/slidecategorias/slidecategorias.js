6f963c5b6a652c13e701059b4168aa09f2702474
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-04-19 15:58:34 -0600

    agregando, las categorias

--- components/slidecategorias/slidecategorias.js
+++ components/slidecategorias/slidecategorias.js
@@ -1,10 +1,23 @@
 export function initializeHeroSlide() {
-    const swiper2 = new Swiper('.mySwiper2', {
-        slidesPerView: 2,
-        spaceBetween: 10,
-        pagination: {
-          el: '.swiper-pagination2',
-          clickable: true,
-        },
-      });
+
+  const categories = [
+    { name: "PC gamer gama alta", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC gamer gama media", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC gamer gama baja", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Pc gamer gama super alta", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Pc para Inteligencia Artificial", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC Gamer para Jugar Call of Duty Black Ops 6", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC Gamer para jugar GTA VI", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC Gamer para Fortnite", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC Gamer para Jugar OverWatch 2", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Computadoras para Edición de Video", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Computadora para Edición de Fotografía", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Workstation para Modelado y Diseño 3D", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Computadoras para Diseño gráfico", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "Computadoras para Programación", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC GAMER", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+    { name: "PC Gamer para Streaming", image: "../../Img/pcgamercdmx.net/MHBJKJVCGVHBJNK.png", link: "#" },
+];
+
+
 }
